af027c0a07aa6dc97132b21bca17ee4907949a4c
Will Nilges <wdn5796@rit.edu>
2023-03-02 00:19:48 -0500

    Holy shit, that worked!?

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 
 // To watch directories
 use notify::{RecommendedWatcher, RecursiveMode, Watcher, Config};
-use std::{path::{Path, PathBuf}, fs};
+use std::{path::{Path, PathBuf}, fs::{self, copy}};
+use file_owner::PathExt;
 
 // To know what directories to watch
 use clap::Parser;
@@ -13,29 +14,33 @@ use std::process::Command;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-   /// Path of dir to watch
-   #[arg(short, long)]
-   home_path: String,
+    /// Path of dir to watch
+    #[arg(short, long)]
+    primary_path: String,
 
-   /// Command to run with path as argument 
-   #[arg(short, long)]
-   command: String,
+    /// Path where we do our work
+    #[arg(short, long)]
+    work_path: String,
+
+    /// Command to run with path as argument 
+    #[arg(short, long)]
+    command: String,
 }
 
 fn main() {
     let args = Args::parse();
     /* FIXME: This could be really dangerous.
-    println!("Clearing {}", &args.home_path);
-    fs::remove_dir_all(&args.home_path);
-    fs::create_dir(&args.home_path);
+    println!("Clearing {}", &args.primary_path);
+    fs::remove_dir_all(&args.primary_path);
+    fs::create_dir(&args.primary_path);
     */
-    println!("watching {}", args.home_path);
-    if let Err(e) = watch(args.home_path, args.command) {
+    println!("watching {}", args.primary_path);
+    if let Err(e) = watch(args.primary_path, args.work_path, args.command) {
         println!("error: {:?}", e)
     }
 }
 
-fn watch<P: AsRef<Path>>(path: P, command: String) -> notify::Result<()> {
+fn watch<P: AsRef<Path>>(path: P, work_path: String, command: String) -> notify::Result<()> {
     let (tx, rx) = std::sync::mpsc::channel();
 
     // Automatically select the best implementation for your platform.
@@ -52,9 +57,10 @@ fn watch<P: AsRef<Path>>(path: P, command: String) -> notify::Result<()> {
                 //println!("changed: {:?}", event);
                 match event.kind {
                     notify::EventKind::Create(notify::event::CreateKind::File) => {
+                        println!("changed: {:?}", event);
                         // TODO: Make this app work with multiple paths at once
-                        println!("{:?}", event.paths);
-                        respond(&command, event.paths);
+                        //println!("{:?}", event.paths); // Debug for seeing event info
+                        respond(&command, &work_path, event.paths);
                     },
                     _ => {
                     }
@@ -66,9 +72,44 @@ fn watch<P: AsRef<Path>>(path: P, command: String) -> notify::Result<()> {
     Ok(())
 }
 
-fn respond(command: &String, files: Vec<PathBuf>) {
-    //fs::create_dir(home_path + "work");
-    let built_command = command.to_owned() + &files[0].display().to_string();
+fn respond(command: &String, work_path: &String, files: Vec<PathBuf>) {
+
+    // Pick the first file created out of there.
+    let first_file = files[0].display().to_string();
+    let first_file_name = files[0].file_name().unwrap().to_string_lossy();
+    let first_file_name_prefix = files[0].file_stem().unwrap().to_string_lossy(); // TODO: Wait for
+    // prefix to get out of nightly so we can use that instead of file_stem()
+    
+    // Get the owner of the path so we can put our output in their homedir.
+    let o = first_file.owner().unwrap();
+    let owner = o.name().unwrap().unwrap();
+    
+    println!("{} Just uploaded a file at {}!", owner, first_file);
+
+    // TODO: Check if it's a video/audio file
+    
+    // TODO: Make a temp directory in our own output directory
+    // MOVE the file over to there, then launch the command on
+    // that path.
+    // workpath=/var/srvrs/work/<FILE_NAME_AND_OWNER>
+    // mkdir $workpath
+    // mv $first_file $workpath
+    
+    // Create temp work directory. We'll put the file here, then run the command we
+    // were given on it.
+    let new_user_work_dir = format!("{}/{}_{}", work_path, owner, first_file_name_prefix);
+    println!("Creating {} for new user work.", new_user_work_dir);
+    fs::create_dir(&new_user_work_dir)
+        .unwrap_or_else(|e| panic!("Error creating dir: {}", e));
+
+    // Move file into temp work directory
+    let new_user_file_path = format!("{}/{}", new_user_work_dir, first_file_name);
+    fs::rename(first_file, &new_user_file_path)
+        .unwrap_or_else(|e| panic!("Error copying file: {}", e));
+
+    println!("Running command!");
+
+    let built_command = command.to_owned() + &new_user_file_path;
     let output = Command::new("sh")
                 .arg("-c")
                 .arg(built_command)
