46c5a0f0a5f363808aff1db90f6a4a56a1645a9f
Will Nilges <wdn5796@rit.edu>
2023-06-23 12:08:03 -0400

    Update whisper container to use CDI

--- ai/whisper/whisper.sh
+++ ai/whisper/whisper.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
+
 WC_PATH=$1 # Location of the video/audio file
+if [[ -z "$WC_PATH" ]]; then echo "Need file as argument"; exit 1; fi
+
 WC_DEVICE="cuda:$2" # GPU to run this on (example "cuda:1")
 if [[ -z "$2" ]]; then WC_DEVICE="cuda:0"; fi # Fallback to GPU 0 if GPU is not provided
-if [[ -z "$WC_PATH" ]]; then echo "Need file as argument"; exit 1; fi
-podman run --rm -it --hooks-dir=/usr/share/containers/oci/hooks.d/ --security-opt=label=disable -v $(dirname $WC_PATH):/workdir:Z -e WHISPER_VIDEO_PATH=$(basename $WC_PATH) -e WHISPER_DEVICE=$WC_DEVICE --name="$(whoami)_$(basename $WC_PATH)" "docker.io/willnilges/webcaptions:v1.0"
+
+NVIDIA_GPU="$2" # Ditto, but of course everything has to be different and unique (example gpu1)
+if [[ -z "$2" ]]; then SD_DEVICE="cuda:0"; NVIDIA_GPU="0"; fi # Fallback to GPU 0 if GPU is not provided
+
+podman run --rm -it  --device nvidia.com/gpu=$NVIDIA_GPU -v $(dirname $WC_PATH):/workdir:Z -e WHISPER_VIDEO_PATH=$(basename $WC_PATH) -e WHISPER_DEVICE=$WC_DEVICE --name="$(whoami)_$(basename $WC_PATH)" "docker.io/willnilges/webcaptions:v1.0"
